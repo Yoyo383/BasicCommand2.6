0b0a81938adfa640169e1e1fc1eef22a9d3e8e2b
yoyo383 <yoad.winter@gmail.com>
2023-11-03 10:34:27 +0200

    documented server.py

--- server.py
+++ server.py
@@ -11,6 +11,13 @@ PORT = 8000
 
 
 def request_to_response(request):
+    """
+    Gets the request and returns the proper response.
+    :param request: The request.
+    :type request: str
+    :return: The proper response.
+    :rtype: str
+    """
     if request == 'TIME':
         now = datetime.now()
         return str(now.hour).zfill(2) + ':' + str(now.minute).zfill(2) + ':' + str(now.second).zfill(2)
@@ -29,21 +36,48 @@ def request_to_response(request):
 
 
 def connect_socket(server_socket):
+    """
+    Binds and connects the socket.
+    :param server_socket: The socket.
+    :type server_socket: socket.socket
+    :return: None.
+    """
     server_socket.bind((IP, PORT))
     server_socket.listen(QUEUE_LEN)
 
 
 def connect_to_client(server_socket):
+    """
+    Waits for client to connect and returns the socket.
+    :param server_socket: The server socket.
+    :type server_socket: socket.socket
+    :return: The client socket.
+    :rtype: socket.socket
+    """
     print('Waiting for client to connect...')
     client_socket, client_addr = server_socket.accept()
     return client_socket
 
 
 def format_response(response):
+    """
+    Formats the response with the byte length.
+    :param response: The response.
+    :type response: str
+    :return: The formatted response.
+    :rtype: str
+    """
     return str(len(response)).zfill(2) + response
 
 
 def main_loop(client_socket):
+    """
+    The loop where the socket waits for a request, gets the proper response, and sends it. If the request is EXIT,
+    the socket closes
+    :param client_socket: The client socket.
+    :type client_socket: socket.socket
+    :return: None.
+    """
     while True:
         request = client_socket.recv(REQUEST_LEN).decode()
 
@@ -59,7 +93,12 @@ def main_loop(client_socket):
 
 
 def client_socket_loop(server_socket):
-    # when closing client socket, wait until another client connects
+    """
+    The server waits for a client and does the main loop. When the client disconnects, the server waits for another one.
+    :param server_socket: The server socket.
+    :type server_socket: socket.socket
+    :return: None.
+    """
     while True:
         client_socket = connect_to_client(server_socket)
 
@@ -75,6 +114,10 @@ def client_socket_loop(server_socket):
 
 
 def main():
+    """
+    The main function.
+    :return: None.
+    """
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
