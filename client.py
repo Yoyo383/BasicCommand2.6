b0d3b8470be5cd84230407e12c215b4dfe7e16e9
yoyo383 <yoad.winter@gmail.com>
2023-11-03 09:58:02 +0200

    separated into functions

--- client.py
+++ client.py
@@ -1,20 +1,38 @@
 import socket
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+IP = '127.0.0.1'
+PORT = 8000
 
-try:
-    server_socket.connect(('127.0.0.1', 8000))
 
+def get_response(server_socket):
+    byte_count = int(server_socket.recv(2).decode())
+    return server_socket.recv(byte_count).decode()
+
+
+def main_loop(server_socket):
     while True:
         server_socket.send(input('> ').encode())
-        byte_count = int(server_socket.recv(2).decode())
-        response = server_socket.recv(byte_count).decode()
+
+        response = get_response(server_socket)
         print(response)
+
         if response == 'Exiting.':
             break
 
-except socket.error as err:
-    print(str(err))
 
-finally:
-    server_socket.close()
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        server_socket.connect((IP, PORT))
+        main_loop(server_socket)
+
+    except socket.error as err:
+        print(str(err))
+
+    finally:
+        server_socket.close()
+
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -4,41 +4,68 @@ import random
 
 QUEUE_LEN = 1
 REQUEST_LEN = 4
-SERVER_NAME = 'the worst server'
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER_NAME = 'the yoyo server'
+
+IP = '127.0.0.1'
+PORT = 8000
 
 
-def get_response(req):
-    if req == 'TIME':
+def request_to_response(request):
+    if request == 'TIME':
         now = datetime.now()
         return str(now.hour).zfill(2) + ':' + str(now.minute).zfill(2) + ':' + str(now.second).zfill(2)
-    elif req == 'NAME':
+
+    elif request == 'NAME':
         return SERVER_NAME
-    elif req == 'RAND':
+
+    elif request == 'RAND':
         return str(random.randint(1, 10))
-    elif req == 'EXIT':
+
+    elif request == 'EXIT':
         return 'Exiting.'
+
     else:
         return 'Invalid command.'
 
 
-try:
-    server_socket.bind(('127.0.0.1', 8000))
+def connect_socket(server_socket):
+    server_socket.bind((IP, PORT))
     server_socket.listen(QUEUE_LEN)
+
+
+def connect_to_client(server_socket):
+    print('Waiting for client to connect...')
+    client_socket, client_addr = server_socket.accept()
+    return client_socket
+
+
+def format_response(response):
+    return str(len(response)).zfill(2) + response
+
+
+def main_loop(client_socket):
     while True:
-        print('Waiting for client to connect...')
-        client_socket, client_addr = server_socket.accept()
+        request = client_socket.recv(REQUEST_LEN).decode()
+
+        response = request_to_response(request)
+        response = format_response(response)
+
+        client_socket.send(response.encode())
+
+        if 'Exiting.' in response:
+            client_socket.close()
+            print('Client disconnected.')
+            break
+
+
+def client_socket_loop(server_socket):
+    # when closing client socket, wait until another client connects
+    while True:
+        client_socket = connect_to_client(server_socket)
 
         try:
             print('Client connected!')
-            while True:
-                request = client_socket.recv(REQUEST_LEN).decode()
-                response = get_response(request)
-                response = str(len(response)).zfill(2) + response
-                client_socket.send(response.encode())
-                if 'Exiting.' in response:
-                    client_socket.close()
-                    break
+            main_loop(client_socket)
 
         except socket.error as err:
             print('client socket error: ' + str(err))
@@ -46,8 +73,20 @@ try:
         finally:
             client_socket.close()
 
-except socket.error as err:
-    print('server socket error: ' + str(err))
 
-finally:
-    server_socket.close()
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        connect_socket(server_socket)
+        client_socket_loop(server_socket)
+
+    except socket.error as err:
+        print('server socket error: ' + str(err))
+
+    finally:
+        server_socket.close()
+
+
+if __name__ == '__main__':
+    main()
