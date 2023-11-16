30559648ad2b72c1bc9c3c732d753727bc5dcf45
yoyo383 <yoad.winter@gmail.com>
2023-11-16 18:42:03 +0200

    added logging + added logs to .gitignore

--- client.py
+++ client.py
@@ -1,10 +1,17 @@
 import socket
+import logging
+import os
 
 IP = '127.0.0.1'
 PORT = 8000
 COMMANDS = ['TIME', 'NAME', 'RAND', 'EXIT']
 
 SERVER_CLOSED_MSG = 'Server has closed, disconnecting.'
+
+LOG_FORMAT = '[%(levelname)s | %(asctime)s | %(processName)s] %(message)s'
+LOG_LEVEL = logging.DEBUG
+LOG_DIR = 'log'
+LOG_FILE = LOG_DIR + '/client.log'
 
 
 def get_response(server_socket):
@@ -51,12 +58,16 @@ def main_loop(server_socket):
 
         if is_cmd_valid(req):
             server_socket.send(req.encode())
+            logging.info(f'Client sent: {req}')
 
             response = get_response(server_socket)
+            logging.info(f'Server sent: {response}')
             print(response)
             if response == SERVER_CLOSED_MSG:
+                logging.debug(f'Server closed, client disconnected.')
                 break
         else:
+            logging.warning(f'Client entered invalid command: {req}')
             print('Invalid command.')
 
 
@@ -66,19 +77,24 @@ def main():
     :return: None.
     """
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    logging.debug('Server socket created.')
 
     try:
         server_socket.connect((IP, PORT))
+        logging.debug('Socket connected to server.')
         main_loop(server_socket)
 
     except socket.error as err:
+        logging.error('Socket error: ' + str(err))
         print('Socket error: ' + str(err))
 
     except KeyboardInterrupt:
         server_socket.send('EXIT'.encode())
+        logging.debug('Keyboard interrupt detected from client, disconnecting from server.')
 
     finally:
         server_socket.close()
+        logging.debug('Server socket closed.')
 
 
 if __name__ == '__main__':
@@ -87,4 +103,9 @@ if __name__ == '__main__':
     assert is_cmd_valid('NAME')
     assert is_cmd_valid('EXIT')
     assert not is_cmd_valid('I HAVE THE HIGH GROUND')
+
+    if not os.path.isdir(LOG_DIR):
+        os.mkdir(LOG_DIR)
+    logging.basicConfig(format=LOG_FORMAT, filename=LOG_FILE, level=LOG_LEVEL)
+
     main()
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
 from datetime import datetime
 import random
+import os
+import logging
 
 QUEUE_LEN = 1
 REQUEST_LEN = 4
@@ -9,6 +11,11 @@ IP = '127.0.0.1'
 PORT = 8000
 
 SERVER_CLOSED_MSG = 'Server has closed, disconnecting.'
+
+LOG_FORMAT = '[%(levelname)s | %(asctime)s | %(processName)s] %(message)s'
+LOG_LEVEL = logging.DEBUG
+LOG_DIR = 'log'
+LOG_FILE = LOG_DIR + '/server.log'
 
 
 def return_time():
@@ -72,7 +79,9 @@ def connect_socket(server_socket):
     :return: None.
     """
     server_socket.bind((IP, PORT))
+    logging.debug(f'Socket bound to: {IP}:{PORT}')
     server_socket.listen(QUEUE_LEN)
+    logging.debug(f'Socket is listening with a queue length of {QUEUE_LEN}.')
 
 
 def connect_to_client(server_socket):
@@ -84,7 +93,10 @@ def connect_to_client(server_socket):
     :rtype: socket.socket
     """
     print('Waiting for client to connect...')
+    logging.debug('Waiting for client to connect...')
     client_socket, client_addr = server_socket.accept()
+    logging.debug('Established connection with client.')
+    print('Client connected!')
     return client_socket
 
 
@@ -105,46 +117,56 @@ def main_loop(client_socket):
     the socket closes
     :param client_socket: The client socket.
     :type client_socket: socket.socket
-    :return: None.
+    :return: Whether the server stays open after the client disconnects (returns False if received KeyboardInterrupt).
+    :rtype: bool
     """
-    req = ''
-    while req != 'EXIT':
-        req = client_socket.recv(REQUEST_LEN).decode()
+    result = True
 
-        response = request_to_response(req)
-        message = protocolize_content(response)
+    try:
+        req = ''
+        while req != 'EXIT':
+            req = client_socket.recv(REQUEST_LEN).decode()
+            logging.info(f'Client sent: {req}')
 
-        client_socket.send(message.encode())
+            response = request_to_response(req)
+            message = protocolize_content(response)
+            logging.debug(f'Response is: {response}, protocol is: {message}.')
 
-    client_socket.close()
-    print('Client disconnected.')
+            client_socket.send(message.encode())
+            logging.info(f'Server sent: {message}')
+
+        client_socket.close()
+        print('Client disconnected.')
+        logging.debug('Client disconnected, closing client socket.')
+
+    except socket.error as err:
+        logging.error('Client socket error: ' + str(err))
+        print('Client socket error: ' + str(err))
+
+    except KeyboardInterrupt:
+        print('Keyboard interrupt detected, closing server.')
+        client_socket.send(protocolize_content(SERVER_CLOSED_MSG).encode())
+        logging.debug('Keyboard interrupt detected, closing server.')
+        result = False
+
+    finally:
+        client_socket.close()
+        logging.debug('Client socket closed.')
+        return result
 
 
 def connect_client_loop(server_socket):
     """
-    The server waits for a client and does the main loop. When the client disconnects, the server waits for another one.
+    The server waits for a client and does the main loop. When the client disconnects, the server waits for another one
+    unless the main loop sent that the server is closing.
     :param server_socket: The server socket.
     :type server_socket: socket.socket
     :return: None.
     """
-    while True:
+    result = True
+    while result:
         client_socket = connect_to_client(server_socket)
-
-        try:
-            print('Client connected!')
-            main_loop(client_socket)
-
-        except socket.error as err:
-            print('Client socket error: ' + str(err))
-
-        except KeyboardInterrupt:
-            print('Keyboard interrupt detected, exiting.')
-            client_socket.send(protocolize_content(SERVER_CLOSED_MSG).encode())
-            client_socket.close()
-            break
-
-        finally:
-            client_socket.close()
+        result = main_loop(client_socket)
 
 
 def main():
@@ -153,19 +175,23 @@ def main():
     :return: None.
     """
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    logging.debug('Server socket created.')
 
     try:
         connect_socket(server_socket)
         connect_client_loop(server_socket)
 
     except socket.error as err:
+        logging.error('Server socket error: ' + str(err))
         print('Server socket error: ' + str(err))
 
     except KeyboardInterrupt:
-        print('Keyboard interrupt detected, exiting.')
+        logging.error('Keyboard interrupt detected, closing server.')
+        print('Keyboard interrupt detected, closing server.')
 
     finally:
         server_socket.close()
+        logging.debug('Server socket closed.')
 
 
 if __name__ == '__main__':
@@ -175,4 +201,9 @@ if __name__ == '__main__':
     assert request_to_response('NAME') == SERVER_NAME
     assert request_to_response('EXIT') == 'Exiting.'
     assert protocolize_content('Do. Or do not. There is no try.') == '31$Do. Or do not. There is no try.'
+
+    if not os.path.isdir(LOG_DIR):
+        os.mkdir(LOG_DIR)
+    logging.basicConfig(format=LOG_FORMAT, filename=LOG_FILE, level=LOG_LEVEL)
+
     main()
